914462aac4652832ceb91bec03c6249ae8c94669
Daniel Morales <danieljosem097@gmail.com>
2023-09-13 20:40:16 -0500

    13-09-23 js

---
+++ main.js
@@ -0,0 +1,20 @@
+// alert("Bienvenidos a mi pagina");
+
+var numero = 1;
+var numero2 = 2;
+
+var nombre = "Daniel";
+
+// alert(numero);
+
+// alert(nombre);
+// alert(typeof nombre);
+
+var nombre = 15;
+// alert(typeof nombre);
+
+function masdos(argumento) {
+  return argumento + 2;
+}
+
+alert(masdos(nombre));
